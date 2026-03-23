4e8570c63591f8af5d6edd2abb64a3d01b4df374
Mitchel ‎ <stanferns2308@gmail.com>
2026-03-23 23:00:54 +0000

    More work on the homepage

--- main screen.py
+++ main screen.py
@@ -7,22 +7,28 @@ Config.set("graphics", "width", "1280")
 Config.set("graphics", "height", "720")
 Config.set("graphics", "resizable", "0")
 
-# Libraries
-import os # For accessing the databases
-import glob # For searching the databases
+# General Libraries
 from kivy.lang import Builder # Builds the KV statement
 from kivymd.app import MDApp # How to actually run the code
 from kivy.properties import StringProperty # For properties that are strings such as MDNavigationRailItemIcoon
-#from kivy.properties import NumericProperty
 from kivymd.uix.navigationrail import MDNavigationRailItem
 from kivymd.uix.list import MDListItem
 from kivy.uix.behaviors import ButtonBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel
+from colour_palette import colourScheme # My colour palette for the GUI
+
+# Libraries for the editor screen
+import os # For accessing the databases
+import glob # For searching the databases
 from fsrs_db_editor import editor_main # My database editor
 import subprocess # How I can open my editor in a separate window
 import sys # Used to get the absolute path of the Python interpreter
-from colour_palette import colourScheme # My colour palette for the GUI
+# Libraries for the shop screen
+
+# Libraries for the task screen
+from kivy.properties import DictProperty
+from spaced_repetition_planner import spaced_repetition_recommendations # My module for the spaced repetition planner
 
 KV = """
 # Template class for the Rail item so that I dont have to repeat stuff multiple times
@@ -104,8 +110,23 @@ KV = """
                 theme_font_name: "Custom"
                 font_name: "robotvar.ttf"
                 
+<SubjectFrame>:
+    size_hint: 1, None
+    #halign: center
+    #pos_hint: root.pos_hint_value
+    #spacing: dp(25)
+    radius: 20
+    height: dp(160)
+    width: self.parent.height *0.45
+    md_bg_color: app.theme_cls.tertiaryColor
+    
+    MDLabel:
+        text: root.text
+        pos_hint: {"center_x": 0.5, "center_y": 0.8}
+        halign: "center"
+        theme_font_name: "Custom"
+        font_name: "robotvar.ttf"
 
-        
 MDBoxLayout:
 
     MDNavigationRail:
@@ -267,12 +288,39 @@ MDBoxLayout:
         # The homepage - where the tasks are shown
         MDScreen:
             name: "home"
-            MDBoxLayout:
-                MDLabel:
-                    text: "Tasks"
-                    halign: "center"
-                    theme_font_name: "Custom"
-                    font_name: "robotvar.ttf"
+            MDAnchorLayout:
+                anchor_x: "center"
+                anchor_y: "center"
+                size_hint: 0.9, 0.9
+                pos_hint: {"center_x": 0.5, "center_y": 0.5}
+                #spacing: dp(25)
+                radius: 20
+                #height: self.parent.height
+                #width: root.height *0.75
+                md_bg_color: app.theme_cls.primaryContainerColor
+                
+                MDScrollView:
+                    scroll_type: ['bars', 'content']
+                    bar_color: app.theme_cls.secondaryColor        
+                    bar_color_inactive: app.theme_cls.secondaryColor
+                    size_hint: 0.9, 0.9
+                    do_scroll_x: False
+                    do_scroll_y: True
+                    bar_width: dp(4)
+                    padding: dp(10)
+
+                    MDBoxLayout:
+                        id: home_list
+                        orientation: "vertical"
+                        size_hint_y: None
+                        height: self.minimum_height
+                        spacing: dp(15)
+                        padding: dp(15)
+                # MDLabel:
+                #     text: "Tasks"
+                #     halign: "center"
+                #     theme_font_name: "Custom"
+                #     font_name: "robotvar.ttf"
         
         # The inventory
         MDScreen:
@@ -316,6 +364,9 @@ class NavItem(MDNavigationRailItem):
 # This defines the class DBItem outside the KV so Kivy understands it
 class DBItem(MDListItem, ButtonBehavior):
     text = StringProperty() # The text used to show the databases' name
+    
+class SubjectFrame(MDBoxLayout):
+    text = StringProperty()
     
 # This defines the EggFrame
 class EggFrame(MDBoxLayout):
@@ -363,6 +414,10 @@ class MainScreen(MDApp):
             # The function on_release sends the ListItem instance as first argument, so capture db separately
             item.bind(on_release=lambda instance, db=db: self.openDB(db)) # 'lambda' is a way for a small function to be defined without a name. There is no main benefit apart from making my (long) code shorter
             self.root.ids.dblist.add_widget(item)
+
+        for db in databases: # 
+            subject_frame = SubjectFrame(text=os.path.splitext(db)[0])
+            self.root.ids.home_list.add_widget(subject_frame)
     
 
 MainScreen().run()
--- spaced_repetition_planner.py
+++ spaced_repetition_planner.py
@@ -1,28 +1,30 @@
-#Spaced repetition planner - the actual recommendation algorithm
+# Spaced repetition planner - the actual recommendation algorithm
 
-#Libraries
+# Libraries
 import sqlite3
 import random
 from datetime import date
 
-# Modulized so that I can call this from a GUI (hopefully this should be much simpler than)
+# Modulised so that I can call this from a GUI (hopefully this should be much simpler than)
 def spaced_repetition_recommendations(table:str):
     if table.endswith(".db"):
         conn = sqlite3.connect(table)
         date_review = (date.today()).isoformat()
         cursor = conn.execute("SELECT TopicDetail FROM Topics WHERE DateToReview <= ?",(date_review,))
         rows = cursor.fetchall()
-        #print(rows)
-        return rows
+        chosen_topics = []
+        all_subject_topics_editable = rows
+        chosen_topics = random.sample(rows, k=min(3, len(rows)))
         conn.close()
+        return chosen_topics
         
-# This is mainly for debugging purposes, I will alter it so that it works by clicking a button
-all_subject_topics=spaced_repetition_recommendations("comp_sci.db")
-chosen_topics = []
-all_subject_topics_editable = all_subject_topics
-chosen_topics = random.sample(all_subject_topics, k=min(2, len(all_subject_topics)))
-print(chosen_topics)
+# # This is mainly for debugging purposes, I will alter it so that it works by clicking a button
+# all_subject_topics=spaced_repetition_recommendations("comp_sci_for_me.db")
+# chosen_topics = []
+# all_subject_topics_editable = all_subject_topics
+# chosen_topics = random.sample(all_subject_topics, k=min(3, len(all_subject_topics)))
+# print(chosen_topics)
 
-def test(table:str):
-    conn = sqlite3.connect(table)
-    conn.execute()
+# def test(table:str):
+#     conn = sqlite3.connect(table)
+#     conn.execute()
