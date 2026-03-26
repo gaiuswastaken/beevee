94b5bd9892b036cc8e50188f65af7a519b90ba87
Mitchel ‎ <stanferns2308@gmail.com>
2026-03-26 13:44:47 +0000

    Finished working on the backend of the index, now need to work on the frontend of the inventory

---
+++ index_manager.py
@@ -0,0 +1,61 @@
+# Libraries
+import sqlite3
+
+# Dictionary Mappings 
+# Hardcoding the dictionary may not be the best solution but it makes accessing it faster 
+# (in the future, may make it a JSON file that way, it is easier to edit but this future solution runs the risk of unwarranted edititng)
+bees = {
+    "Basic Bee": {"rarity": "Common"},
+    "Bumble Bee": {"rarity": "Rare"},
+    "Stubborn Bee": {"rarity": "Rare"},
+    "Bubble Bee": {"rarity": "Epic"},
+    "Rage Bee": {"rarity": "Epic"},
+    "Exhausted Bee": {"rarity": "Epic"},
+    "Baby Bee": {"rarity": "Legendary"},
+    "Lion Bee": {"rarity": "Legendary"},
+    "Spicy Bee": {"rarity": "Mythic"}
+}
+
+# Creates the bees inventory
+def create_index():
+    conn = sqlite3.connect("index.db")
+    cursor = conn.cursor()
+    # Also conducts a sanity check to see if there isnt a table called index (this prevents unnecessary overwriting of data, which could lead to data loss)
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS beeIndex (
+                beeName TEXT PRIMARY KEY,
+                beeRarity TEXT NOT NULL,
+                discovered TEXT NOT NULL
+            )
+        ''')
+    conn.commit()
+    conn.close()
+    
+
+# Allows for the index to be built when the onboarding screen is launched
+def build_index():
+    conn = sqlite3.connect("index.db")
+    cursor = conn.cursor()
+    for bee in range(1,len(bees)):
+        bee = list(bees.keys())[bee]
+        values = [(bee, bees[bee]["rarity"], str(False))]
+        cursor.executemany("INSERT INTO beeIndex (beeName, beeRarity, discovered) VALUES (?, ?, ?)", values)
+    conn.commit()
+    conn.close()
+
+# Updates the value of the bees to be discovered to True
+def discover_bee(bee):
+    conn = sqlite3.connect("index.db")
+    cursor = conn.cursor()
+    cursor.execute("UPDATE beeIndex SET discovered = ? WHERE beeName = ?", (str(True), bee))
+    conn.commit()
+    conn.close()
+
+# Returns the list of bees - useful for the Index page in the main screen as I will separate them from discovered and undiscovered
+def get_bees():
+    conn = sqlite3.connect("index.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT beeName FROM beeIndex")
+    available_bees = cursor.fetchall()
+    conn.close()
+    return available_bees
--- inventory_manager.py
+++ inventory_manager.py
@@ -16,9 +16,11 @@ bees = {
     "Spicy Bee": {"rarity": "Mythic"}
 }
 
+# Creates the bees inventory
 def create_inventory():
     conn = sqlite3.connect("inventory.db")
     cursor = conn.cursor()
+    # Also conducts a sanity check to see if there isnt a table called inventory (this prevents unnecessary overwriting of data, which could lead to data loss)
     cursor.execute('''
             CREATE TABLE IF NOT EXISTS inventory (
                 beeName TEXT PRIMARY KEY,
@@ -30,15 +32,19 @@ def create_inventory():
     conn.close()
     
 
+# Allows for any bee to be added
 def add_bee_to_inventory(bee):
     conn = sqlite3.connect("inventory.db")
     cursor = conn.cursor()
+    # Checks if the bee is already present
     object_bee_in_db_search = cursor.execute('''SELECT count FROM inventory WHERE beeName = (?)''', (bee,))
+    # Converts the bee object into text where it can be validated and manipulated
     text_bee_in_db_search = object_bee_in_db_search.fetchone()
-    if text_bee_in_db_search == None:
-        cursor.execute("UPDATE inventory SET beeName = ?, rarity = ? , count = 1", (bee,))
-
+    if text_bee_in_db_search == None: # If the bee is not present
+        print(bees[bee]["rarity"])
+        values = [(bee, bees[bee]["rarity"], 1)]
+        cursor.executemany("INSERT INTO inventory (beeName, beeRarity, count) VALUES (?, ?, ?)", values)
+    else: # Bee is present
+        cursor.execute("UPDATE inventory SET count = count + 1 WHERE beeName = ? ", (bee,))
+    conn.commit()
     conn.close()
-
-create_inventory()
-add_bee_to_inventory("Basic Bee")
