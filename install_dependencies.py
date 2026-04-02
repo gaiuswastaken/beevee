8125143870c8abc9386edd2f6e812bcf8134aa77
Mitchel ‎ <stanferns2308@gmail.com>
2026-04-02 23:12:25 +0100

    Modified install script to use subprocess.run instead of os.system()

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,4 +1,4 @@
-import os # Executes shell commands (such as pip)
+from subprocess import run # Executes shell commands (such as pip)
 import sys # Gets the current interpreter being used to run the script
 import importlib # Checks the libraries have been installed
 
@@ -8,22 +8,21 @@ def check_and_install_dependencies_not_kivymd():
     for i in range (len(dependencies)):
         try: # Tries to import it
             # Force installs Kivy 2.3.1 regardless of install state
-            os.system(f"{sys.executable} -m pip install kivy==2.3.1")
+            run([sys.executable, "-m", "pip", "install", "kivy==2.3.1"])
             importlib.import_module(dependencies[i])
             print(f"{dependencies[i]} is installed")
         except ImportError: # If module is not found, hence giving an import error
             if dependencies[i] == "kivy":
-                os.system(f"{sys.executable} -m pip install kivy==2.3.1")
+                run([sys.executable, "-m", "pip", "install", "kivy==2.3.1"])
             else:
-                os.system(f"{sys.executable} -m pip install {dependencies[i]}")
+                run([sys.executable, "-m", "pip", "install", dependencies[i]])
 
 def check_and_install_kivymd():
     try: # Tries to import it
         importlib.import_module("kivymd")
         print("KivyMD is installed")
     except ImportError: # If module is not found, hence giving an import error
-        os.system(f"{sys.executable} -m pip install https://github.com/kivymd/KivyMD/archive/master.zip")
-        
+        run([sys.executable, "-m", "pip", "install", "https://github.com/kivymd/KivyMD/archive/master.zip"])
 
 check_and_install_dependencies_not_kivymd()
 check_and_install_kivymd()
